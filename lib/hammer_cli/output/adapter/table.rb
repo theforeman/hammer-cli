ea3318414f66434e8dea78d21ed5fa4f07b248a2
Martin Bačovský <mbacovsk@redhat.com>
2013-08-27 17:36:48 +0200

    fixes #2971 - Convert all output to be tabular

--- lib/hammer_cli/output/adapter/table.rb
+++ lib/hammer_cli/output/adapter/table.rb
@@ -1,23 +1,53 @@
-require 'terminal-table'
+require 'table_print'
 
 module HammerCLI::Output::Adapter
-  class Table < Abstract
+
+  class Table < Base
 
     def print_records fields, data, heading=nil
 
-      headings = fields.collect{|f| f.label.to_s}
       rows = data.collect do |d|
-        fields.collect do |f|
-          f.get_value(d).to_s
+        row = {}
+        fields.each do |f|
+          row[f.label.to_sym] = f.get_value(d)
         end
+        row
       end
 
-      table = Terminal::Table.new :title => heading,
-                                  :headings => headings,
-                                  :rows => rows,
-                                  :style => { :border_y => '', :border_i => '', :border_x => '-' }
-      puts table
+      options = fields.collect do |f|
+        { f.label.to_sym => { 
+            :formatters => [ Formatter.new(self, "format_"+field_type(f.class)) ] } }
+      end
+
+      printer = TablePrint::Printer.new(rows, options)
+      output = printer.table_print
+      dashes = /\n(-+)\n/.match output
+      puts dashes[1]
+      puts output
+      puts dashes[1]
+    end
+
+    def print_heading heading, size
+      size = heading.size if heading.size > size
+      puts '-' * size
+      puts ' ' * ((size-heading.size)/2) + heading
+      puts '-' * size
     end
 
   end
+
+  class Formatter
+    def initialize adapter, method
+      @adapter = adapter
+      @method = method
+    end
+
+    def format value
+      if @adapter.respond_to?(@method, true)
+        value = @adapter.send(@method, value) 
+      end
+      value
+    end
+  end
+
 end
