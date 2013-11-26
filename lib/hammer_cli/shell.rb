1e8bfaeeedc9df63549dd8632e877aa6f57571e0
Tomas Strachota <tstrachota@redhat.com>
2013-11-26 17:32:27 +0100

    shell - filter commands and options

--- lib/hammer_cli/shell.rb
+++ lib/hammer_cli/shell.rb
@@ -3,6 +3,39 @@ require 'readline'
 
 module HammerCLI
 
+  class ShellMainCommand < AbstractCommand
+
+    class HelpCommand < AbstractCommand
+      command_name "help"
+      desc "Print help for commands"
+
+      parameter "[COMMAND] ...", "command"
+
+      def execute
+        ShellMainCommand.run('', command_list << '-h')
+        HammerCLI::EX_OK
+      end
+    end
+
+    class ExitCommand < AbstractCommand
+      command_name "exit"
+      desc "Exit interactive shell"
+
+      def execute
+        exit HammerCLI::EX_OK
+      end
+    end
+
+    def self.load_commands(main_cls)
+      cmds = main_cls.recognised_subcommands.select do |sub_cmd|
+        !(sub_cmd.subcommand_class <= HammerCLI::ShellCommand)
+      end
+      self.recognised_subcommands.push(*cmds)
+    end
+
+    autoload_subcommands
+  end
+
   class ShellCommand < AbstractCommand
 
     def execute
@@ -12,12 +45,11 @@ module HammerCLI
 
       stty_save = `stty -g`.chomp
 
-
       begin
-        puts "Welcome to the hammer interactive shell"
-        puts "Type 'help' for usage information"
-        while line = Readline.readline('hammer> ', true)
-          HammerCLI::MainCommand.run('', line.split) unless line.start_with? 'shell' or line.strip.empty?
+        print_welcome_message
+        ShellMainCommand.load_commands(HammerCLI::MainCommand)
+        while line = Readline.readline(prompt, true)
+          ShellMainCommand.run('', line.split) unless line.start_with? 'shell' or line.strip.empty?
         end
       rescue Interrupt => e
         puts
@@ -27,6 +59,15 @@ module HammerCLI
     end
 
     private
+
+    def prompt
+      'hammer> '
+    end
+
+    def print_welcome_message
+      puts "Welcome to the hammer interactive shell"
+      puts "Type 'help' for usage information"
+    end
 
     def common_prefix(results)
       results.delete_if{ |r| !r[0].start_with?(results[0][0][0]) }.length == results.length
@@ -48,5 +89,5 @@ module HammerCLI
 
   end
 
-  HammerCLI::MainCommand.subcommand "shell", "Interactive Shell", HammerCLI::ShellCommand
+  HammerCLI::MainCommand.subcommand "shell", "Interactive shell", HammerCLI::ShellCommand
 end
