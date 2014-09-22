e520c7cdacf8174d98ce7a7d9382f3d6f719da5a
Martin Bačovský <martin.bacovsky@gmail.com>
2014-09-22 13:28:26 +0200

    Fixes #7566 - added support for dependeces among modules

--- lib/hammer_cli/modules.rb
+++ lib/hammer_cli/modules.rb
@@ -1,16 +1,67 @@
-
+require 'tsort'
 module HammerCLI
+
+  class ModulesList < Hash
+    include TSort
+
+    def tsort_each_node(&block)
+      each_key.sort.each(&block)
+    end
+
+    def tsort_each_child(node, &block)
+      fetch(node).each(&block)
+    end
+  end
 
   class Modules
 
     def self.names
+      # add dependencies
+      modules = find_dependencies({}, enabled_modules)
+      # sort with the deps in mind
+      ModulesList[modules].tsort
+    rescue TSort::Cyclic => e
+      raise HammerCLI::ModuleCircularDependency.new(_("Unable to load modules: Circular dependency detected (%s)") % e.message)
+    end
 
+
+    def self.find_dependencies(dependencies, module_list)
+      new_deps = []
+
+      # add inspected modules in current level (depth)
+      dependencies.merge(Hash[module_list.map{ |m| [m, []] }])
+
+      # lookup dependencies
+      module_list.each do |mod|
+        deps = dependencies_for(mod)
+        logger.debug(_("Module depenedency detected: %{mod} requires %{deps}") % { :mod => mod, :deps => deps.join(', ') }) unless deps.empty?
+        dependencies[mod] = deps # update deps
+        # check new/disabled deps
+        deps.each do |dep|
+          if !dependencies.has_key?(dep)
+            if HammerCLI::Settings.get(dep.gsub(/^hammer_cli_/, ''), :enable_module) == false
+              raise HammerCLI::ModuleDisabledButRequired.new(_("Module %{mod} depends on module %{dep} which is disabled in configuration") % { :mod => mod, :dep => dep })
+            end
+            new_deps << dep
+          end
+        end
+      end
+      dependencies = find_dependencies(dependencies, new_deps) unless new_deps.empty?
+      dependencies
+    end
+
+    def self.dependencies_for(module_name)
+      mod = Gem::Specification.find_by_name(module_name)
+      mod.dependencies.select{ |dep| dep.name =~ /^hammer_cli_.*/ }.map(&:name)
+    end
+
+    def self.enabled_modules
       # legacy modules config
       modules = HammerCLI::Settings.get(:modules) || []
       logger.warn _("Legacy configuration of modules detected. Check section about configuration in user manual") unless modules.empty?
 
       HammerCLI::Settings.dump.inject(modules) do |names, (mod_name, mod_config)|
-        if mod_config.kind_of?(Hash) && !mod_config[:enable_module].nil?
+        if is_module_config?(mod_config)
           mod = ["hammer_cli_#{mod_name}"]
           if mod_config[:enable_module]
             names += mod
@@ -67,6 +118,11 @@ module HammerCLI
     end
 
     protected
+
+    def self.is_module_config?(config)
+      config.kind_of?(Hash) && !config[:enable_module].nil?
+    end
+
     def self.logger
       Logging.logger['Modules']
     end
