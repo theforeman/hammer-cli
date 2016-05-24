f4f2218c3c3dbc9ad0fb587f9bb821f0b0b1ff51
Martin Bačovský <martin.bacovsky@gmail.com>
2016-05-24 14:44:43 +0200

    Some refactoring

--- lib/hammer_cli/defaults.rb
+++ lib/hammer_cli/defaults.rb
@@ -23,27 +23,24 @@ module HammerCLI
     end
 
     def delete_default_from_conf(param)
-      conf_file = YAML.load_file(path)
-      conf_file[:defaults].delete_if { |k,| k.to_s.gsub('-','_') == param.to_s.gsub('-','_') }
-      write_to_file conf_file
-      conf_file
+      update_defaults_file do |defaults|
+        defaults.delete_if { |k,| defaults_match?(k, param) }
+      end
     end
 
     def add_defaults_to_conf(default_options, provider)
       create_default_file if defaults_settings.empty?
-      defaults = YAML.load_file(path)
-      defaults[:defaults] ||= {}
-      default_options.each do |key, value|
-        key = key.to_sym
-        defaults[:defaults].delete_if { |k,| k.to_s.gsub('-','_') == key.to_s.gsub('-','_') }
-        defaults[:defaults][key] = (value ? {:value => value,} : {:provider => provider})
+      update_defaults_file do |defaults|
+        default_options.each do |key, value|
+          key = key.to_sym
+          defaults.delete_if { |k,| defaults_match?(k, key) }
+          defaults[key] = (value ? {:value => value,} : {:provider => provider})
+        end
       end
-      write_to_file defaults
-      defaults
     end
 
     def defaults_set?(param)
-      defaults_settings.keys.any? { |k| k.to_s.gsub('-','_') == param.to_s.gsub('-','_') }
+      defaults_settings.keys.any? { |k| defaults_match?(k, param) }
     end
 
     def get_defaults(opt)
@@ -79,6 +76,20 @@ module HammerCLI
       else
         raise DefaultsPathError.new(_("Couldn't create %s please create the path before defaults are enabled.") % path)
       end
+    end
+
+    def update_defaults_file
+      conf_file = YAML.load_file(@path)
+      conf_file[:defaults] ||= {}
+      yield conf_file[:defaults]
+      write_to_file conf_file
+      conf_file
+    end
+
+    private
+
+    def defaults_match?(default_a, default_b)
+      default_a.to_s.gsub('-','_') == default_b.to_s.gsub('-','_')
     end
   end
 
