faa54edd4e8a04d6a17c274c24f8dd8e39b14f83
Tomas Strachota <tstrachota@redhat.com>
2017-09-22 12:51:06 +0200

    Revert "Fixes #20804 - Support for deprecating commands"
    This reverts commit 3b8ca9c23ce1269a646bae34e6b9516980c9649c.

--- lib/hammer_cli/subcommand.rb
+++ lib/hammer_cli/subcommand.rb
@@ -3,34 +3,14 @@ module HammerCLI
 
   module Subcommand
 
-    class Definition < Clamp::Subcommand::Definition
+    class LazyDefinition < Clamp::Subcommand::Definition
 
-      def initialize(names, description, subcommand_class, options = {})
+      def initialize(names, description, subcommand_class_name, path)
         @names = Array(names)
         @description = description
-        @subcommand_class = subcommand_class
-        @hidden = options[:hidden]
-        @warning = options[:warning]
-      end
-
-      def hidden?
-        @hidden
-      end
-
-      def subcommand_class
-        warn(@warning) if @warning
-        @subcommand_class
-      end
-
-      attr_reader :warning
-    end
-
-    class LazyDefinition < Definition
-
-      def initialize(names, description, subcommand_class_name, path, options = {})
-        super(names, description, subcommand_class_name, options)
-        @loaded = false
+        @subcommand_class_name = subcommand_class_name
         @path = path
+        @loaded = false
       end
 
       def loaded?
@@ -38,14 +18,14 @@ module HammerCLI
       end
 
       def subcommand_class
-        warn(@warning) if @warning
         if !@loaded
           require @path
           @loaded = true
-          @constantized_class = @subcommand_class.constantize
+          @constantized_class = @subcommand_class_name.constantize
         end
         @constantized_class
       end
+
     end
 
     def self.included(base)
@@ -64,29 +44,13 @@ module HammerCLI
         end
       end
 
-      def subcommand!(name, description, subcommand_class = self, options = {}, &block)
+      def subcommand!(name, description, subcommand_class = self, &block)
         remove_subcommand(name)
-        subcommand(name, description, subcommand_class, options, &block)
+        subcommand(name, description, subcommand_class, &block)
         logger.info "subcommand #{name} (#{subcommand_class}) was created."
       end
 
-      def subcommand(name, description, subcommand_class = self, options = {}, &block)
-        definition = Definition.new(name, description, subcommand_class, options)
-        define_subcommand(name, subcommand_class, definition, &block)
-      end
-
-      def lazy_subcommand(name, description, subcommand_class_name, path, options = {})
-        definition = LazyDefinition.new(name, description, subcommand_class_name, path, options)
-        define_subcommand(name, Class, definition)
-      end
-
-      def lazy_subcommand!(name, description, subcommand_class_name, path, options = {})
-        remove_subcommand(name)
-        self.lazy_subcommand(name, description, subcommand_class_name, path, options)
-        logger.info "subcommand #{name} (#{subcommand_class_name}) was created."
-      end
-
-      def define_subcommand(name, subcommand_class, definition, &block)
+      def subcommand(name, description, subcommand_class = self, &block)
         existing = find_subcommand(name)
         if existing
           raise HammerCLI::CommandConflict, _("can't replace subcommand %<name>s (%<existing_class>s) with %<name>s (%<new_class>s)") % {
@@ -95,10 +59,23 @@ module HammerCLI
             :new_class => subcommand_class
           }
         end
-        subcommand_class = Class.new(subcommand_class, &block) if block
-        declare_subcommand_parameters unless has_subcommands?
-        recognised_subcommands << definition
+        super
       end
+
+      def lazy_subcommand(name, description, subcommand_class, path)
+        # call original subcommand to ensure command's parameters are set correctly
+        # (hammer command SUBCOMMAND [ARGS] ...)
+        subcommand(name, description, Class)
+        # replace last subcommand definition with correct lazy-loaded one
+        recognised_subcommands[-1] = LazyDefinition.new(name, description, subcommand_class, path)
+      end
+
+      def lazy_subcommand!(name, description, subcommand_class, path)
+        remove_subcommand(name)
+        self.lazy_subcommand(name, description, subcommand_class, path)
+        logger.info "subcommand #{name} (#{subcommand_class}) was created."
+      end
+
     end
 
   end
