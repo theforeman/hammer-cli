85f77d84e56ac668afabdac5f0fa88c20b5c04a6
d-m-u <drewuhlmann@gmail.com>
2018-02-12 08:00:52 -0500

    Fixes #21359
    Hammer no longer supports ruby 2.0, there's no point keeping the old code.

--- test/unit/i18n_test.rb
+++ test/unit/i18n_test.rb
@@ -40,52 +40,23 @@ describe HammerCLI::I18n do
   let(:domain2) { TestLocaleDomain.new('domain2', true) }
   let(:unavailable_domain) { TestLocaleDomain.new('domain3', false) }
 
-  # skip the tests on older versions of fast_gettext (ruby 2.0)
-  if FastGettext::VERSION >= '1.2.0'
-    describe "with fast_gettext >= 1.2.0" do
-      it "creates base merge repository" do
-        HammerCLI::I18n.translation_repository.class.must_equal FastGettext::TranslationRepository::Merge
-      end
-
-      it "registers available domains at gettext" do
-        repo = mock
-        FastGettext::TranslationRepository.expects(:build).with(domain1.domain_name,
-          :path => domain1.locale_dir,
-          :type => domain1.type,
-          :report_warning => false).returns(repo)
-
-        HammerCLI::I18n.translation_repository.expects(:add_repo).with(repo)
-        HammerCLI::I18n.add_domain(domain1)
-      end
-
-      it "skips registering domains that are not available" do
-        HammerCLI::I18n.add_domain(domain1)
-        HammerCLI::I18n.add_domain(domain2)
-        HammerCLI::I18n.add_domain(unavailable_domain)
-        HammerCLI::I18n.domains.must_equal [domain1, domain2]
-      end
-    end
-  end
-
-  describe "with fast_gettext < 1.2.0" do
-    let(:fast_gettext_version) { '1.1.0' }
-
-    it "creates base chain repository" do
-      HammerCLI::I18n.translation_repository.class.must_equal FastGettext::TranslationRepository::Chain
+  describe 'with fast_gettext >= 1.2.0' do
+    it 'creates base merge repository' do
+      HammerCLI::I18n.translation_repository.class.must_equal FastGettext::TranslationRepository::Merge
     end
 
-    it "registers available domains at gettext" do
+    it 'registers available domains at gettext' do
       repo = mock
       FastGettext::TranslationRepository.expects(:build).with(domain1.domain_name,
         :path => domain1.locale_dir,
         :type => domain1.type,
         :report_warning => false).returns(repo)
 
-      HammerCLI::I18n.translation_repository.chain.expects(:<<).with(repo)
+      HammerCLI::I18n.translation_repository.expects(:add_repo).with(repo)
       HammerCLI::I18n.add_domain(domain1)
     end
 
-    it "skips registering domains that are not available" do
+    it 'skips registering domains that are not available' do
       HammerCLI::I18n.add_domain(domain1)
       HammerCLI::I18n.add_domain(domain2)
       HammerCLI::I18n.add_domain(unavailable_domain)
@@ -93,4 +64,3 @@ describe HammerCLI::I18n do
     end
   end
 end
-
