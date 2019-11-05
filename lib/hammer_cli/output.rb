4b09b72b79866b1459b6de74a92746f49bca9cf8
Oleh Fedorenko <ofedoren@redhat.com>
2019-11-05 14:42:23 +0100

    Fixes #27318 - Extract table generator into reusable component (#314)

--- lib/hammer_cli/output.rb
+++ lib/hammer_cli/output.rb
@@ -1,9 +1,9 @@
 require File.join(File.dirname(__FILE__), 'output/output')
 require File.join(File.dirname(__FILE__), 'output/fields')
 require File.join(File.dirname(__FILE__), 'output/formatters')
+require File.join(File.dirname(__FILE__), 'output/generators')
 require File.join(File.dirname(__FILE__), 'output/adapter')
 require File.join(File.dirname(__FILE__), 'output/definition')
 require File.join(File.dirname(__FILE__), 'output/dsl')
 require File.join(File.dirname(__FILE__), 'output/record_collection')
 require File.join(File.dirname(__FILE__), 'output/field_filter')
-
