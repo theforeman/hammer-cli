theforeman/hammer-cli

commit f1b2e8b67bef81f26894f159171da6f7f55a456b

fixes #18439 - only include .mo files below locale/

hammer_cli.gemspec
@@ -17,7 +17,11 @@ Gem::Specification.new do |s|
 Hammer cli provides universal extendable CLI interface for ruby apps
 EOF
 
-  s.files            = Dir['{lib,test,bin,doc,config,locale}/**/*', 'LICENSE', 'README*', 'hammer_cli_complete'] + ['man/hammer.1.gz']
+  locales = Dir['locale/*'].select { |f| File.directory?(f) }
+  s.files = Dir['{lib,test,bin,doc,config}/**/*', 'LICENSE', 'README*', 'hammer_cli_complete'] +
+    locales.map { |loc| "#{loc}/LC_MESSAGES/hammer-cli.mo" } +
+    ['man/hammer.1.gz']
+
   s.test_files       = Dir['test/**/*']
   s.extra_rdoc_files = Dir['{doc,config}/**/*', 'README*']
   s.require_paths = ["lib"]

commit b5fa081886f037da0233fe0cb3df9e37dfb1e523

Include Makefile.def in the gem
In 649d103e861852c22fa68710cdb7be67c0c7e266 infrastructure was added to
make translation management reusable. Later this was broken in
f1b2e8b67bef81f26894f159171da6f7f55a456b when the gemspec was trimmed to
only include .mo files. That meant that other Hammer plugins couldn't
actually use the infrastructure.

Fixes: f1b2e8b67bef81f26894f159171da6f7f55a456b

hammer_cli.gemspec
@@ -20,7 +20,7 @@ EOF
   locales = Dir['locale/*'].select { |f| File.directory?(f) }
   s.files = Dir['{lib,test,bin,doc,config}/**/*', 'LICENSE', 'README*'] +
     locales.map { |loc| "#{loc}/LC_MESSAGES/hammer-cli.mo" } +
-    ['man/hammer.1.gz']
+    ['locale/Makefile.def', 'man/hammer.1.gz']
 
   s.test_files       = Dir['test/**/*']
   s.extra_rdoc_files = Dir['{doc,config}/**/*', 'README*']