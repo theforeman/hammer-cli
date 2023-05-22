51e273f6e7b4bf4e2dd452086a46982502594664
Oleh Fedorenko <ofedoren@redhat.com>
2023-05-22 20:44:21 +0530

    Refs #36283 - Improve Rakefile tx commands

--- lib/hammer_cli/i18n/find_task.rb
+++ lib/hammer_cli/i18n/find_task.rb
@@ -1,11 +1,12 @@
 require 'rake'
-require 'fileutils'
 require_relative '../task_helper'
 
 module HammerCLI
   module I18n
     class FindTask
       include Rake::DSL
+
+      MIN_TRANSLATION_PERC = 50
 
       def initialize(domain, version)
         @domain = domain
@@ -40,21 +41,21 @@ module HammerCLI
             lang_percentages = stats['data'].each_with_object({}) do |lang, res|
               res[lang['id'].split(':').last] = (lang['attributes']['translated_strings'] * 100.0 / lang['attributes']['total_strings']).round
             end
-            lang_percentages.select { |_, v| v >= 50 }.each_key do |lang|
-              lang_dir = File.expand_path("#{@domain.locale_dir}/#{lang}")
+            lang_percentages.select { |_, v| v >= MIN_TRANSLATION_PERC }.each_key do |lang|
+              lang_dir = File.join(@domain.locale_dir, lang)
               FileUtils.mkpath(lang_dir)
-              FileUtils.cp(File.expand_path("#{@domain.locale_dir}/#{@domain.domain_name}.pot", __dir__), "#{lang_dir}/#{@domain.domain_name}.po")
+              FileUtils.cp(File.join(@domain.locale_dir, "#{@domain.domain_name}.pot"), File.join(lang_dir, "#{@domain.domain_name}.po"))
             end
           end
         end
 
         namespace :tx do
-          desc 'Pull translaions from transifex'
+          desc 'Pull translations from transifex'
           task :pull do
             raise 'Command tx not found. Make sure you have transifex-client installed and configured.' unless system("command -v tx >/dev/null 2>&1")
 
             sh "tx pull -f"
-            edit_files = Dir.glob(File.expand_path("#{@domain.locale_dir}/**/*.edit.po", __dir__))
+            edit_files = Dir.glob(File.join(@domain.locale_dir, '**', '*.edit.po'))
             edit_files.each do |edit_file|
               `sed -i 's/^\\("Project-Id-Version: \\).*$/\\1#{@domain.domain_name} #{@version}\\\\n"/' #{edit_file};`
             end
@@ -62,20 +63,20 @@ module HammerCLI
 
           desc 'Merge .edit.po into .po'
           task :update_po do
-            edit_files = Dir.glob(File.expand_path("#{@domain.locale_dir}/**/*.edit.po", __dir__))
+            edit_files = Dir.glob(File.join(@domain.locale_dir, '**', '*.edit.po'))
             edit_files.each do |edit_file|
               po_file = edit_file.gsub('.edit.po', '.po')
-              `msgcat --use-first --no-location #{edit_file} #{po_file} > #{po_file}`
+              sh "msgcat --use-first --no-location #{edit_file} #{po_file} --output-file #{po_file}"
             end
           end
 
           desc 'Generate MO files from PO files'
           task :all_mo do
-            po_files = Dir.glob(File.expand_path("#{@domain.locale_dir}/**/#{@domain.domain_name}.po", __dir__))
+            po_files = Dir.glob(File.join(@domain.locale_dir, '**', "#{@domain.domain_name}.po"))
             po_files.each do |po_file|
               dir = File.dirname(po_file) + '/LC_MESSAGES'
               FileUtils.mkdir_p(dir)
-              `msgfmt -o #{dir}/#{@domain.domain_name}.mo #{po_file}`
+              sh "msgfmt -o #{dir}/#{@domain.domain_name}.mo #{po_file}"
             end
           end
 
@@ -87,8 +88,8 @@ module HammerCLI
             Rake::Task['tx:update_po'].invoke
             Rake::Task['tx:all_mo'].invoke
             locale_dir = File.expand_path(@domain.locale_dir, __dir__)
-            `git add #{locale_dir}`
-            `git commit -m "i18n - extracting new, pulling from tx"`
+            sh "git add #{locale_dir}"
+            sh 'git commit -m "i18n - extracting new, pulling from tx"'
             puts 'Changes commited!'
           end
 
@@ -96,23 +97,26 @@ module HammerCLI
           task :check do
             raise 'Command pofilter not found. Make sure you have translate-toolkit installed.' unless system("command -v pofilter >/dev/null 2>&1")
 
-            po_files = Dir.glob(File.expand_path("#{@domain.locale_dir}/**/#{@domain.domain_name}.po", __dir__))
+            po_files = Dir.glob(File.join(@domain.locale_dir, '**',"#{@domain.domain_name}.po"))
             po_files.each do |po_file|
               pox_file = po_file.gsub('.po', '.pox')
-              `msgfmt -c #{po_file}`
-              `pofilter --nofuzzy -t variables -t blank -t urls -t emails -t long -t newlines -t endwhitespace -t endpunc \
-                -t puncspacing -t options -t printf -t validchars --gnome #{po_file} > #{pox_file};`
-              `cat #{pox_file}`
-              `! grep -q msgid #{pox_file}`
+              sh "msgfmt -c #{po_file}"
+              sh "pofilter --nofuzzy -t variables -t blank -t urls -t emails -t long -t newlines -t endwhitespace -t endpunc \
+                -t puncspacing -t options -t printf -t validchars --gnome #{po_file} > #{pox_file};"
+              sh("! grep -q msgid #{pox_file}") do |ok, _|
+                sh "cat #{pox_file}" unless ok
+                abort "See errors above for #{pox_file}."
+              end
             end
           end
 
           desc 'Clean everything, removes *.edit.po, *.po.timestamp and *.pox files'
           task :clean do
-            edit_files = Dir.glob(File.expand_path("#{@domain.locale_dir}/**/*.edit.po", __dir__))
-            timestamp_files = Dir.glob(File.expand_path("#{@domain.locale_dir}/**/*.po.time_stamp", __dir__))
-            pox_files = Dir.glob(File.expand_path("#{@domain.locale_dir}/**/*.pox", __dir__))
-            FileUtils.rm_f(edit_files + timestamp_files + pox_files)
+            edit_files = Dir.glob(File.join(@domain.locale_dir, '**', '*.edit.po'))
+            timestamp_files = Dir.glob(File.join(@domain.locale_dir, '**', '*.po.time_stamp'))
+            pox_files = Dir.glob(File.join(@domain.locale_dir, '**', '*.pox'))
+            messages_file = Dir.glob(File.join(@domain.locale_dir, '..', 'messages.mo'))
+            FileUtils.rm_f(edit_files + timestamp_files + pox_files + messages_file)
           end
         end
       end
